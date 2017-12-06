9ee47ca6d41f3ef23e4049ff56f8b0907c405287
Frederic Le <le000132@algonquinlive.com>
2017-12-05 21:41:14 -0500

    Empty Unit Testing Methods for Database created.

---
+++ app/src/test/java/com/frankmeana/foodengine/DatabaseHelperTest.java
@@ -0,0 +1,38 @@
+package com.frankmeana.foodengine;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MinhHien on 05-Dec-17.
+ */
+public class DatabaseHelperTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+    
+    @Test
+    public void insertData() throws Exception {
+
+    }
+
+    @Test
+    public void select() throws Exception {
+
+    }
+
+    @Test
+    public void resetTable() throws Exception {
+
+    }
+
+}
